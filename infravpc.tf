ba132faba76e571f4ac3385def6ab4da6fe8f9dd
kiokathir <109990536+kiokathir@users.noreply.github.com>
2025-07-27 22:52:27 +0530

    Update infravpc.tf

--- infravpc.tf
+++ infravpc.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_subnet"{
     cidr_block="10.0.1.0/24"
     map_public_ip_on_launch = true
     availability_zone="ap-south-1a"
-    tags{
+    tags={
         Name = "Pub-Sub"
     }
 }
@@ -31,7 +31,7 @@ resource "aws_subnet" "private_subnet"{
     vpc_id=aws_vpc.Infra_vpc.id
     cidr_block="10.0.2.0/24"
     availability_zone="ap-south-1a"
-    tags{
+    tags={
         Name = "Pri-Sub"
     }
 }
